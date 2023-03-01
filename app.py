530615aec92f00e1aaae35522fe630c8c80f1031
Rishab Kumar <45825464+rishabkumar7@users.noreply.github.com>
2023-03-01 10:14:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 app = Flask(__name__)
 
+
+@app.route('/')
+def hello_world():
+    return 'Ahoy, World!'
+  
 @app.route("/sms", methods=['POST'])
 def dalle2():
   """get incoming message"""
